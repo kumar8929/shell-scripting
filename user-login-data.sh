6fe428cee8ff055b23f0850fafbbe3d739fb0487
kishore yb <37611142+kumar8929@users.noreply.github.com>
2025-04-20 19:25:49 +0100

    this script generates user login details

--- user-login-data.sh
+++ user-login-data.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#create a new directory if not exsists
+Mkdir -p /root/Users_Login_Data/
+#get data in a variable
+DATE=`date +’%m-%d-%y-%H-%M’`
+#log the user Data in afile
+Who | awk ‘{print $1,$3,$4,$5}’ > /root/ Users_Login_Data/User_$DATE.txt
 
